ffbc6f810b6e701bfea1e80a8194b6bd3dd6028c
kyulimii <ckr8305@naver.com>
2026-08-05 16:09:16 +0900

    refactor: cors 필터 수정

--- src/main/java/org/example/community/global/config/WebConfig.java
+++ src/main/java/org/example/community/global/config/WebConfig.java
@@ -44,7 +44,7 @@ public class WebConfig implements WebMvcConfigurer {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
-                "https://ayla-community.kro.kr"
+                "https://ayla-community.store"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
